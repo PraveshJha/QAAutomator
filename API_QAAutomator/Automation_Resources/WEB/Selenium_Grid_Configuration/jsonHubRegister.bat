4aae46283a9f67d87e7a80d10809a90f9b9b4ec6
Pravesh Jha <pravesh28@gmail.com>
2021-10-15 13:39:56 +0530

    Update
    Update

--- API_QAAutomator/Automation_Resources/WEB/Selenium_Grid_Configuration/jsonHubRegister.bat
+++ API_QAAutomator/Automation_Resources/WEB/Selenium_Grid_Configuration/jsonHubRegister.bat
@@ -1,6 +1,6 @@
 REM //############ Prerequisite ########################################
 REM //############ Start Hub ########################################
-SET HomePath=D:\Git_QAAutomator\API_Code\API_QAAutomatorCodeBase\AutomationStudioAPI\AutomationStudioAPI\Automation_Resources\WEB\Selenium_Grid_Configuration\
+SET HomePath=C:\QAAutomator\QAAutomator\API_QAAutomator\Automation_Resources\WEB\Selenium_Grid_Configuration\
 SET Severname=%HomePath%selenium-server-standalone-3.141.59.jar
 start cmd /k Call java -jar %Severname% -role hub -hubConfig %HomePath%HubConfig.json
 
--- Fronthand_QAAutomator/ServerAddress.js
+++ Fronthand_QAAutomator/ServerAddress.js
@@ -1,3 +1,3 @@
 window.ENV = {
-     "APIURL": "https://apiqaautomator.herokuapp.com/"
+     "APIURL": "http://localhost:4001/api/"
  }
