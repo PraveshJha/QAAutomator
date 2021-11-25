d42eb3b505f4305dc46cfec1e30b886c0097c66e
Pravesh Jha <pravesh28@gmail.com>
2021-11-25 23:05:23 +0530

    Update
    Update

--- API_QAAutomator/Automation_Resources/WEB/Selenium_Grid_Configuration/jsonNodeRegister.bat
+++ API_QAAutomator/Automation_Resources/WEB/Selenium_Grid_Configuration/jsonNodeRegister.bat
@@ -1,6 +1,6 @@
 REM //############Staring Chrome node on Port 5555 ########################################
-SET HomePath=C:\QAAutomator-Solution\API_QAAutomator\Automation_Resources\WEB\Selenium_Grid_Configuration\
-SET MiscPath=C:\QAAutomator-Solution\API_QAAutomator\Automation_Resources\WEB\SeleniumDLL\
+SET HomePath=D:\QAAutomatorFileSystem\Automation_Resources\WEB\Selenium_Grid_Configuration\
+SET MiscPath=D:\QAAutomatorFileSystem\Automation_Resources\WEB\SeleniumDLL\
 SET Severname=%HomePath%selenium-server-standalone-3.141.59.jar
 start cmd /k Call java -Dwebdriver.chrome.driver=%MiscPath%/chromedriver.exe -Dwebdriver.gecko.driver=%MiscPath%/geckodriver.exe -Dwebdriver.ie.driver=%MiscPath%/IEDriverServer.exe -jar %Severname% -role node -nodeConfig %HomePath%NodeConfig.json
 
